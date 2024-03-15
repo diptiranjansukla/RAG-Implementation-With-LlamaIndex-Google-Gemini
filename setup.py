b2a6fbdb235bdee0568d39b3e08eeca8f373bd4d
diptiranjansukla <diptiranjanshukla1998@gmail.com>
2024-03-15 23:08:20 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'QApplication',
+    version= '0.0.1',
+    author= 'diptiranjan',
+    author_email= 'diptiranjansukla36@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
